d2367330137d413aa3a75e85f88e8318013c695e
nebstarmalala <nebstartrizzle@gmail.com>
2020-09-30 19:54:36 +0300

    update msg sender

--- sms_sender.py
+++ sms_sender.py
@@ -6,6 +6,7 @@ api_key = "732c67db06ce4d51ed0afa83a689da10456d8c98781c2399c3dc7858eee2d247"
 africastalking.initialize(username, api_key)
 sms = africastalking.SMS
 
+
 # response  = sms.send("Hello Msg", ["+254793723138"])
 # print(response)
 
